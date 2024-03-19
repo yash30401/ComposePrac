5cfa4ba5859d83e95763ff65a07cef03df592c63
Yashveer singh <yashveersingh30401@gmail.com>
2024-03-19 13:35:22 +0530

    State

--- app/src/main/java/com/yash/composeprac/MainActivity.kt
+++ app/src/main/java/com/yash/composeprac/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,6 +41,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    val extraPadding = if(expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -44,13 +53,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Row(modifier = modifier.padding(24.dp)) {
             Column(
                 modifier = modifier.weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
 
-            ElevatedButton(onClick = {}) {
-                Text(text = "Show More")
+            ElevatedButton(onClick = {
+                expanded = !expanded
+            }) {
+                Text(if(expanded) "Show Less" else "Show More")
             }
         }
     }
