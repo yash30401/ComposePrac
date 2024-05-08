01e42676bbc6620926b57096b86c16867a6bfcb7
Yashveer singh <yashveersingh30401@gmail.com>
2024-05-08 22:27:11 +0530

    starting State in compose

---
+++ app/src/main/java/com/yash/composeprac/stateincompose/WellnessScreen.kt
@@ -0,0 +1,9 @@
+package com.yash.composeprac.stateincompose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier){
+    WellnessCounter(modifier)
+}
---
+++ app/src/main/java/com/yash/composeprac/stateincompose/Wellnesscounter.kt
@@ -0,0 +1,9 @@
+package com.yash.composeprac.stateincompose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessCounter(modifier: Modifier){
+
+}
