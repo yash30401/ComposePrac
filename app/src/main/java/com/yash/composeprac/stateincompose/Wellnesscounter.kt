5653ec14c6abffe006b1e75e04087ea24933ddd9
Yashveer singh <yashveersingh30401@gmail.com>
2024-05-09 13:59:34 +0530

    Stateless and statefull statehoisting

--- app/src/main/java/com/yash/composeprac/stateincompose/Wellnesscounter.kt
+++ app/src/main/java/com/yash/composeprac/stateincompose/Wellnesscounter.kt
@@ -4,27 +4,31 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnrememberedMutableState", "RememberReturnType")
 @Composable
-fun WellnessCounter(modifier: Modifier) {
+fun StatefullCounter(modifier: Modifier) {
     var count by rememberSaveable {
         mutableStateOf(0)
     }
+    StatelessCounter(count = count, modifier = modifier) {
+        count++
+    }
+
+}
+
+@Composable
+private fun StatelessCounter(count: Int, modifier: Modifier = Modifier, onIncrement: () -> Unit) {
+
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
             Text(
@@ -32,18 +36,17 @@ fun WellnessCounter(modifier: Modifier) {
             )
         }
         Button(
-            onClick = { count++ },
+            onClick = { onIncrement },
             enabled = count < 10,
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text(text = "Add One")
         }
     }
-
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun WellnessCounterPreview(modifier: Modifier = Modifier) {
-    WellnessCounter(modifier = modifier)
+    StatefullCounter(modifier = modifier)
 }
