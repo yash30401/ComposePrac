cd04f27da6d69638b83a7a5e007d06930e9d3997
Yashveer singh <yashveersingh30401@gmail.com>
2024-05-09 13:49:34 +0530

    RememberSaveable

--- app/src/main/java/com/yash/composeprac/stateincompose/Wellnesscounter.kt
+++ app/src/main/java/com/yash/composeprac/stateincompose/Wellnesscounter.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,7 +22,7 @@ import androidx.compose.ui.unit.dp
 @SuppressLint("UnrememberedMutableState", "RememberReturnType")
 @Composable
 fun WellnessCounter(modifier: Modifier) {
-    var count by remember {
+    var count by rememberSaveable {
         mutableStateOf(0)
     }
     Column(modifier = modifier.padding(16.dp)) {
