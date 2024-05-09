4c78c54f200e13dffbf3d3b1954ec8b0e1540ce5
Yashveer singh <yashveersingh30401@gmail.com>
2024-05-09 14:22:55 +0530

    WellnessTaskItem created

---
+++ app/src/main/java/com/yash/composeprac/wellness/WellnessTaskItem.kt
@@ -0,0 +1,46 @@
+package com.yash.composeprac.wellness
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = taskName, modifier = Modifier
+                .weight(1f)
+                .padding(16.dp)
+        )
+        Checkbox(checked = checked, onCheckedChange = onCheckedChange)
+        IconButton(onClick = { onClose }) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun WellnessTaskItemPreview(modifier: Modifier = Modifier) {
+    WellnessTaskItem("Task 1", false, {
+
+    }, {
+
+    }, modifier)
+}
